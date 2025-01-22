0bd4d9f4752f365838ff0562233cfbda62e75668
Goulielmos Floros <goulielmos13@hotmail.com>
2025-01-22 18:12:24 +0000

    app-start

---
+++ index.js
@@ -0,0 +1,142 @@
+#!/usr/bin/env node
+import * as fs from "fs"
+import * as path from "path"
+import * as OBC from "@thatopen/components"
+import * as env from "dotenv"
+import { CohereClient } from 'cohere-ai';
+import * as prompt from '@inquirer/prompts';
+
+function getKey() {
+  env.config()
+  return process.env.APIKEY
+}
+
+function initAI(apiKey) {
+  return new CohereClient({
+    token: apiKey
+  })
+}
+
+function readFile(joinedPath) {
+  return fs.readFileSync(joinedPath)
+}
+
+function getFiles() {
+	// Get the last argument from the vector (the path to the files)
+  const input = process.argv[2]
+
+	// Indicate usage
+  if (!input) {
+    throw new Error('Usage: bcfai-chat <path/to/bcf/files>')
+  }
+
+  let files
+  try {
+	  // Read directory and filter needed files
+    files = fs.readdirSync(
+      input
+    ).filter(file => path.extname(file).toLowerCase() === '.bcf')
+  } catch {
+      throw new Error('Insert a valid directory.')
+  }
+
+  if (files.length === 0) {
+      throw new Error(`No BCF files found in ${input}`)
+  }
+
+	// Return the full path of the file
+  return files.map(file => path.join(input, file))
+}
+
+// Instantiate the engine and get the topics component
+function getComponents() {
+  const components = new OBC.Components()
+  return components.get(OBC.BCFTopics)
+}
+
+// De data comes from the readFile function
+// Gets turned into a int array and loaded to the engine.
+async function loadTopics(data) {
+  const bcf = getComponents()
+  const buffer = new Uint8Array(data)
+  const bcfData = await bcf.load(buffer)
+  return JSON.stringify(bcfData.topics)
+}
+
+async function makeRequest(stringData) {
+
+  // Include an array to store the messages.
+  const messages = []
+  const key = getKey()
+  const cohere = initAI(key)
+  
+  // This is the structure needed for input prompt
+  const userPrompt = {
+    name: 'prompt', // A name
+    type: 'string', // The type of the input
+    message: 'Write your prompt:' // Message to be displayed
+  }
+
+  while (true) {
+	  // There are other options to use with prompt but this one works just fine.
+    const input = await prompt.input(userPrompt)
+
+    if (!input) {
+      console.log('No input given, try writing something.')
+      continue
+    }
+    
+    // This prompt can be tuned, but it works good to 
+    // get the conversation with cohere going
+    // stringData is the information from the topics.
+    const coherePrompt = {
+      message: `Based on the following data ${stringData}
+      You should only create the response based on the information given.
+      The data contains this structure, that does not mean that you are going to give me this
+      {
+        title: string
+        description: string
+        type: string
+        priority: string
+        labels: Array
+        stage: string
+        assignee: string
+      }
+      Information that is not found on ${stringData} should not be presented on the
+      result
+      your job is to answer the following question: ${input}.
+      If the question is empty, say that you can't process empty questions and to try again.`
+    }
+    const response = await cohere.chat(coherePrompt)
+  
+    console.log(response.text)
+
+    // Push an object in this format to the messages array
+    messages.push(
+      {
+        user: question,
+        cohere: responseText
+      }
+    )
+
+    const fileName = 'CohereChat.json'
+    const stringifiedMessages = JSON.stringify(messages, null, 2)
+    fs.writeFileSync(fileName, stringifiedMessages)
+  
+    console.log(`Chat exported to ${process.cwd()}\\\\${fileName}`)
+  }
+}
+
+async function chat() {
+  const filesPath = getFiles()
+  let stringData
+
+  for (const file of filesPath) {
+    const fileData = readFile(file)
+    stringData = await loadTopics(fileData)
+  }
+
+  await makeRequest(stringData)
+}
+
+await chat()
